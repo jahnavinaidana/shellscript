e90e387ded886cb5804fc76536c624cf4ac0827f
Your Name <you@example.com>
2025-01-06 16:47:42 -0700

    awsproject

---
+++ 03-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=srinu #no space between = and value
+person2=vamsi
+
+echo "$person1:: hey get up let's go out"
+echo "$person2:: no i want to sleep"
+echo "$person1:: how much more time are you gonna sleep?"
+echo "$person2:: i don't know and i don't want to come out, you go"
+echo "$person1:: you lazy ass, get up or else i'll throw water on you"
+echo "$person2:: urgh! what the hell is your problem?"
