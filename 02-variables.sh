94a61211d4a21d4940f13bfa8b37c886f44789a8
Your Name <you@example.com>
2025-01-06 16:32:00 -0700

    awsproject

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "srinu:: hey get up let's go out"
+echo "vamsi:: no i want to sleep"
+echo "srinu:: how much more time are you gonna sleep?"
+echo "vamsi:: i don't know and i don't want to come out, you go"
+echo "srinu:: you lazy ass, get up or else i'll throw water on you"
+echo "vamsi:: urgh! what the hell is your problem?" 
