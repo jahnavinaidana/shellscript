bfbde64bed0d47f55739baf092a27009173e0396
Your Name <you@example.com>
2025-01-06 18:35:45 -0700

    awsproject

---
+++ 04-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#declaring of variable
+person1=$1
+person2=$2
+
+#referring variable
+echo "$person1:: hey get up let's go out"
+echo "$person2:: no i want to sleep"
+echo "$person1:: how much more time are you gonna sleep?"
+echo "$person2:: i don't know and i don't want to come out, you go"
+echo "$person1:: you lazy ass, get up or else i'll throw water on you"
+echo "$person2:: urgh! what the hell is your problem?"
