d9cadee0184fee919f22975b2c30707d0c37e524
Your Name <you@example.com>
2025-01-07 18:11:56 -0700

    awsproject

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -6,3 +6,7 @@ echo "script name: $0"
 echo "Current Working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "process ID of the current shell script: $$"
+sleep 60 &
+echo "proces id of last background command: $!"
