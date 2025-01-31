37e2de606861da977d739c75a7e78ff2db8dfb31
Your Name <you@example.com>
2025-01-30 18:47:41 -0700

    awsproject

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,8 +2,8 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
-LOGFILE =/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
