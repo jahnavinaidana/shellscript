e9a85d0e80b92eb1c8dd2a8896a7820f789a2ca8
Your Name <you@example.com>
2025-02-24 14:32:07 -0700

    awsproject

---
+++ 15-currentscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE ="Devops from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "process ID of current shell script: $$"
+
+./16-otherscript.sh
+
+echo "after calling other script, course: $COURSE"
