1b4628a04efc26b562a51a4ccc7a1233c902d179
Your Name <you@example.com>
2025-02-24 14:35:20 -0700

    awsproject

--- 15-currentscript.sh
+++ 15-currentscript.sh
@@ -5,6 +5,6 @@ COURSE ="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "process ID of current shell script: $$"
 
-./16-otherscript.sh
+#./16-otherscript.sh
 
 echo "after calling other script, course: $COURSE"
